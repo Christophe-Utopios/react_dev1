63c6214737281ef9922a281ec6ee6257ecd4a319
Christophe-Ringot <christophe.ringot1996@gmail.com>
2025-10-29 10:49:48 +0100

    demo liste

--- demos/01_demo_base/src/components/FirstComponent/FirstComponent.jsx
+++ demos/01_demo_base/src/components/FirstComponent/FirstComponent.jsx
@@ -8,12 +8,27 @@ const FirstComponent = () => {
     return "Je suis " + nom;
   };
 
+  const myPersons = [
+    { firstname: "Toto", age: 18 },
+    { firstname: "Toto2", age: 20 },
+    { firstname: "Toto3", age: 28 },
+    { firstname: "Toto4", age: 38 },
+    { firstname: "Toto5", age: 48 },
+  ];
+
   return (
     <>
       <div>
         <h1 className={classes.paragrapheRouge}>Mon premier composant</h1>
         <p className={classes.paragrapheRouge}>Je suis {nom}</p>
         <p>{maFonction()}</p>
+        <ul>
+          {myPersons.map((person, index) => (
+            <li>
+              {index} | Prénom : {person.firstname} - Age : {person.age}
+            </li>
+          ))}
+        </ul>
       </div>
     </>
   );
